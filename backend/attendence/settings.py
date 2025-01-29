79a01cf9b4514b5adfa1c9bcfccb8236ec4ae0ff
iksanh <iksanhariji@gmail.com>
2025-01-29 10:40:54 +0800

    menambahkan model pegawai

--- backend/attendence/settings.py
+++ backend/attendence/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #APPS 
+    'pegawai',
 
     #LIBRARY
     'rest_framework'
--- backend/pegawai/models.py
+++ backend/pegawai/models.py
@@ -3,3 +3,12 @@ from django.db import models
 # Create your models here.
 
 
+#model for jabatan 
+class Pegawai(models.Model):
+    nip = models.CharField(max_length=20, unique=True)
+    nama = models.CharField(max_length=50)
+    jabatan = models.CharField(max_length=50)
+    grade = models.IntegerField()
+
+    class Meta:
+        db_table = "m_pegawai"
