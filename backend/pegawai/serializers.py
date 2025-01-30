7025edff46b9cca3f024067958f859c8463fbac5
iksanh <iksanhariji@gmail.com>
2025-01-30 10:57:13 +0800

    menambahkan serializer pegawai

---
+++ backend/pegawai/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Pegawai
+
+
+class PegawaiSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Pegawai
+        fields = '__all__'
