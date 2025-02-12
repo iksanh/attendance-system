58a80f219a8a8a3fe93d19908883f9ccdb1b38d0
iksanh <iksanhariji@gmail.com>
2025-02-13 07:04:40 +0800

    menambahkan model absensi detail

--- backend/absensi/models.py
+++ backend/absensi/models.py
@@ -7,6 +7,21 @@ class Absensi (models.Model):
     pegawai  = models.ForeignKey(Pegawai, on_delete=models.CASCADE)
     date = models.DateField()
 
-
     class Meta:
         db_table  = "m_absensi"
+
+class MetodeAbsensi(models.TextChoices):
+    EOFFICE = "e-office","E-Office"
+    FINGER = "finger", "Finger Print"
+    MANUAL = "manual","Manual"
+
+class AbsensiDetail(models.Model):
+
+    absensi  = models.ForeignKey(Absensi, on_delete=models.CASCADE)
+    metode = models.CharField(max_length=30, choices=MetodeAbsensi.choices, default=MetodeAbsensi.MANUAL)
+    waktu_masuk = models.TimeField(null=True, blank=True)
+    waktu_keluar = models.TimeField(null=True, blank=True)
+    jam_kerja = models.FloatField(null=True, blank=True)
+    
+    class Meta: 
+        db_table = "m_absensi_detail"
